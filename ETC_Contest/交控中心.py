29e883c359f5e8c2e5993b25aefc33d493fd3bd0
Kay Zhang <kayforeveronly@gmail.com>
2020-07-22 12:36:06 +0800

    SMS Data in South has been sorted out

--- ETC_Contest/交控中心.py
+++ ETC_Contest/交控中心.py
@@ -1,14 +1,49 @@
 import xlrd
 import pandas as pd
 import re
-import datetime
+from datetime import datetime
+
 
 pd.reset_option("max_columns")
 
 df_TrafficControl = pd.read_excel("/Users/kayzhang/Downloads/第6屆ETC創意競賽資料/108年高公局所轄各分局事故簡訊資料/108年_南區交控中心.xlsx")
-df_TrafficControl['通報級數'] = df_TrafficControl['簡訊內容'].str.split(',').str.get(0).str[:6]
-df_TrafficControl['簡訊日期'] = df_TrafficControl['簡訊內容'].str.split(',').str.get(0).str[6:]
-df_TrafficControl['簡訊時間'] = df_TrafficControl['簡訊內容'].str.split(',').str.get(1).str[:5]
+
+dataLength = len(df_TrafficControl['簡訊內容'])
+
+
+# 級數
+content = []
+for i in range(dataLength):
+    content.append(re.findall(r'.*通報\d級', df_TrafficControl['簡訊內容'][i]))
+content = ([j for con in content for j in con])
+df_TrafficControl['通報級數'] = pd.Series(content)
+
+
+# 時間
+smsDate = []
+startTime = []
+endTime = []
+for i in range(dataLength):
+    smsDate.append(re.findall(r'\d\d/\d\d', df_TrafficControl['簡訊內容'][i]))
+    startTime.append(re.findall(r'\d\d:\d\d', df_TrafficControl['簡訊內容'][i])[0])
+    endTime.append(re.findall(r'\d\d:\d\d', df_TrafficControl['簡訊內容'][i])[-1])
+smsDate = ([j for sms in smsDate for j in sms])
+df_TrafficControl['簡訊日期'] = pd.Series(smsDate)
+df_TrafficControl['簡訊時間'] = pd.Series(startTime)
+df_TrafficControl['事故排除時間'] = pd.Series(endTime)
+
+df_TrafficControl['簡訊時間_cul'] = pd.Series(startTime)
+df_TrafficControl['事故排除時間_cul'] = pd.Series(endTime)
+df_TrafficControl['處理耗時'] = pd.Series(endTime) # 先隨便給個符合dataFrame的格式
+
+for i in range(dataLength):
+    df_TrafficControl['簡訊時間_cul'][i] = datetime.strptime(df_TrafficControl['簡訊時間'][i], "%H:%M")
+    df_TrafficControl['事故排除時間_cul'][i] = datetime.strptime(df_TrafficControl['事故排除時間'][i], "%H:%M")
+    df_TrafficControl['處理耗時'][i] = (df_TrafficControl['事故排除時間_cul'][i] - df_TrafficControl['簡訊時間_cul'][i])
+
+df_TrafficControl = df_TrafficControl.drop(['簡訊時間_cul'], axis=1)
+df_TrafficControl = df_TrafficControl.drop(['事故排除時間_cul'], axis=1)
+
 
 roadName = []
 for road in df_TrafficControl['簡訊內容']:
@@ -16,8 +51,9 @@ for road in df_TrafficControl['簡訊內容']:
 roadName = ([i for road in roadName for i in road])
 df_TrafficControl['公路名稱'] = pd.Series(roadName)
 
+
 direction = []
-for direct in range(len(df_TrafficControl['簡訊內容'])):
+for direct in range(dataLength):
     if re.findall(r'[東西南北][上下向 ]', df_TrafficControl['簡訊內容'][direct]):
         direction.append(re.findall(r'[東西南北][上下向 ]', df_TrafficControl['簡訊內容'][direct]))
     else:
@@ -25,14 +61,50 @@ for direct in range(len(df_TrafficControl['簡訊內容'])):
 direction = ([i for direct in direction for i in direct])
 df_TrafficControl['向車道'] = pd.Series(direction)
 
-solveTime = []
-for time in range(len(df_TrafficControl['簡訊內容'])):
-    solveTime.append(re.findall(r'\w\w:\w\w', df_TrafficControl['簡訊內容'][time])[-1])
-df_TrafficControl['事故排除時間'] = pd.Series(solveTime)
+
+hurt = []
+for i in range(dataLength):
+    if re.findall(r'[無\d+]人受傷', df_TrafficControl['簡訊內容'][i]):
+        hurt.append(re.findall(r'[無\d+]人受傷', df_TrafficControl['簡訊內容'][i]))
+    else:
+        hurt.append(['0'])
+hurt = ([i for j in hurt for i in j])
+df_TrafficControl['受傷人數'] = pd.Series(hurt)
+for i in range(dataLength):
+    if df_TrafficControl['受傷人數'][i] == '無人受傷':
+        df_TrafficControl['受傷人數'][i] = 0
+    elif df_TrafficControl['受傷人數'][i] == '0':
+        df_TrafficControl['受傷人數'][i] = 0
+    else:
+        df_TrafficControl['受傷人數'][i] = re.findall(r'\d+', df_TrafficControl['受傷人數'][i])[0]
+
+dead = []
+for i in range(dataLength):
+    if re.findall(r'[無\d+]人死亡', df_TrafficControl['簡訊內容'][i]):
+        dead.append(re.findall(r'[無\d+]人死亡', df_TrafficControl['簡訊內容'][i]))
+    else:
+        dead.append(['0'])
+dead = ([i for j in dead for i in j])
+df_TrafficControl['死亡人數'] = pd.Series(dead)
+for i in range(dataLength):
+    if df_TrafficControl['死亡人數'][i] == '無人死亡':
+        df_TrafficControl['死亡人數'][i] = 0
+    elif df_TrafficControl['死亡人數'][i] == '0':
+        df_TrafficControl['死亡人數'][i] = 0
+    else:
+        df_TrafficControl['死亡人數'][i] = re.findall(r'\d+', df_TrafficControl['死亡人數'][i])[0]
+
+kilometers = []
+for i in range(dataLength):
+    if re.findall(r'回堵約\d+K', df_TrafficControl['簡訊內容'][i], re.IGNORECASE):
+        kilometers.append(re.findall(r'\d+K', df_TrafficControl['簡訊內容'][i], re.IGNORECASE)[-1])
+        kilometers_notK = []
+        for j in kilometers:
+            kilometers_notK.append(j.upper().strip('K'))
+    else:
+        kilometers.append('0')
+df_TrafficControl['回堵公里數(K)'] = pd.Series(kilometers_notK)
 
 
-# df_TrafficControl['Col3'] = df_TrafficControl['簡訊內容'].str.split(',').str.get(2)
 df_TrafficControl = df_TrafficControl.drop(['簡訊內容'], axis=1)
-# print(df_TrafficControl.head(30))
-print(df_TrafficControl.info())
-
+print(df_TrafficControl.head(60))
