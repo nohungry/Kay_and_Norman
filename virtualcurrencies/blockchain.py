16b66500a3481722a6d29762f7060f80a2ea2907
Kay Zhang <kayforeveronly@gmail.com>
2020-11-28 22:17:17 +0800

    Lesson 2: connect to the block

--- virtualcurrencies/blockchain.py
+++ virtualcurrencies/blockchain.py
@@ -1,4 +1,5 @@
 from hashlib import sha256
+import json
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -11,16 +12,40 @@ class Block:
     # helper function to help us calculate the hash of curre
     def calculate_hash(self):
         # convert all block attribute to string
-        step1 = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
-        step2 = step1.encode()
-        step3 = sha256(step2)
-        final_result = step3.hexdigest()
-        return final_result
+        block_info = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        return sha256(block_info.encode()).hexdigest()
 
-myblock1 = Block(1, "11/17/20:26", "amount: 1000USD", "")
-myblock2 = Block(2, "11/17/20:30", "amount: 2000USD", myblock1.hash)
-print(myblock1.hash)
-print(myblock2.previous_hash)
 
-# deploy
-# cloud service
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()] # arraylist of size 1
+    
+    def create_genesis_block(self):
+        genesis_block = Block(0, "11/24/20:17", "Genesis!!", "0")
+        return genesis_block
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.chain[-1].hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+
+# myblock1 = Block(1, "11/17/20:26", "amount: 1000USD", "")
+# myblock2 = Block(2, "11/17/20:30", "amount: 2000USD", myblock1.hash)
+# print(myblock1.hash)
+# print(myblock2.previous_hash)
+
+# Main execution/driver code
+kay_coin = Blockchain()
+
+data1 = '{"amount": "100USD", "sender": "Elliot", "receiver": "Whiterose"}'
+data2 = '{"amount": "200USD", "sender": "Whiterose", "receiver": "Tyrell"}'
+
+block1 = Block(1, "11/25", json.loads(data1), "")
+block2 = Block(2, "11/25", json.loads(data2), "")
+
+kay_coin.add_block(block1)
+kay_coin.add_block(block2)
+
+for block in kay_coin.chain:
+    print(json.dumps(eval(str(block.__dict__)), indent=2))
