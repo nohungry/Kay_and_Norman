c00a6c27089d9b2424fe532768fdd6b617a0790b
Kay Zhang <kayforeveronly@gmail.com>
2020-12-01 21:30:17 +0800

    Lesson 3:prove of work

--- virtualcurrencies/blockchain.py
+++ virtualcurrencies/blockchain.py
@@ -7,18 +7,28 @@ class Block:
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
+        self.nonce = 0
         self.hash = self.calculate_hash()
 
     # helper function to help us calculate the hash of curre
     def calculate_hash(self):
         # convert all block attribute to string
-        block_info = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        block_info = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)
         return sha256(block_info.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        while (self.hash[0:difficulty] != "0" * difficulty):
+            print("Trial #", self.nonce, ":", self.hash)
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        print("Total trials:", self.nonce)
+        print("Block mined:", self.hash)
 
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()] # arraylist of size 1
+        self.difficulty = 5
     
     def create_genesis_block(self):
         genesis_block = Block(0, "11/24/20:17", "Genesis!!", "0")
@@ -26,7 +36,7 @@ class Blockchain:
 
     def add_block(self, new_block):
         new_block.previous_hash = self.chain[-1].hash
-        new_block.hash = new_block.calculate_hash()
+        new_block.mine_block(self.difficulty)
         self.chain.append(new_block)
 
 
